ccd167c1ff06084b41510b9c32a0d34f4cc8b7c9
YuriyKrasilnikov <yury.krasilnikov@gmail.com>
2020-09-20 20:31:20 +0300

    add cni to istio

--- src/grpc_greeter_server/src/app.py
+++ src/grpc_greeter_server/src/app.py
@@ -1,4 +1,5 @@
 """The Python implementation of the GRPC helloworld.Greeter server."""
+import os
 import _thread
 import autoreloader
 from grpc_server import Server
@@ -8,4 +9,4 @@ import sys, os, time, logging
 _thread.start_new_thread(autoreloader.reloader_thread, (__name__,))
 
 if __name__ == '__main__':
-  Server(server_port=9000)
+  Server(server_port=9000, cassandra_key=os.environ['cassandra-key'])
